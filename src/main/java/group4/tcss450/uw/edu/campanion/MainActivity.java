31c13fdf8d4f1088271c4f7fae9f343546c51f3c
AmandaAldrich <amlaldrich@gmail.com>
2017-11-01 21:35:36 -0700

    Add files via upload
    Simple Front end to login and signup

--- src/main/java/group4/tcss450/uw/edu/campanion/MainActivity.java
+++ src/main/java/group4/tcss450/uw/edu/campanion/MainActivity.java
@@ -1,25 +1,115 @@
+/**
+ * Amanda Aldrich
+ *
+ * Main Activity for Login and Register Capabilities
+ */
+
 package group4.tcss450.uw.edu.campanion;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ChoiceFragment.OnFragmentInteractionListener, LoginFragment.OnFragmentInteractionListener, SignupFragment.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if(savedInstanceState == null) {
+            if (findViewById(R.id.fragmentContainer) != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.fragmentContainer, new ChoiceFragment())
+                        .commit();
+            }
+        }
     }
+
+
+    //calls login or register depending on button press
     @Override
-    protected void onStart(){
-        super.onStart();
+    public void onFragmentInteraction(ChoiceFragment.Choice choice) {
+
+        switch (choice){
+            case  LOGIN:
+
+                LoginFragment loginFragment;
+                loginFragment = new LoginFragment();
+                Bundle args = new Bundle();
+                args.putSerializable(getString(R.string.choice_key), choice);
+                loginFragment.setArguments(args);
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, loginFragment)
+                        .addToBackStack(null);
+
+
+                // Commit the transaction
+                transaction.commit();
+                //do things
+
+                break;
+
+            case  REGISTER:
+
+                SignupFragment signupFragment;
+                signupFragment = new SignupFragment();
+                Bundle args1 = new Bundle();
+                args1.putSerializable(getString(R.string.choice_key), choice);
+                signupFragment.setArguments(args1);
+                FragmentTransaction transaction1 = getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, signupFragment)
+                        .addToBackStack(null);
+
+
+                // Commit the transaction
+                transaction1.commit();
+
+                break;
+
+        }
     }
 
+
+    //login hands off to our placeholder for now
     @Override
-    protected void onRestart(){
-        super.onRestart();
+    public void onLoginFragmentInteraction(String login, String password) {
+
+        User user = new User();
+        user.setLogin(login);
+        user.setPassword(password);
+
+
+        PlaceHolderFragment displayFragment;
+        displayFragment = new PlaceHolderFragment();
+        Bundle args2 = new Bundle();
+        args2.putSerializable(getString(R.string.login_key), user);
+        displayFragment.setArguments(args2);
+        FragmentTransaction transaction2 = getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, displayFragment)
+                .addToBackStack(null);
+
+        // Commit the transaction
+        transaction2.commit();
+
     }
 
+    //signup hands off to our login for now
+    @Override
+    public void onSignupFragmentInteraction(String login, String password) {
 
+        User user = new User();
+        user.setLogin(login);
+        user.setPassword(password);
+
+        PlaceHolderFragment displayFragment;
+        displayFragment = new PlaceHolderFragment();
+        Bundle args3 = new Bundle();
+        args3.putSerializable(getString(R.string.signup_key), user);
+        displayFragment.setArguments(args3);
+        FragmentTransaction transaction3 = getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, displayFragment)
+                .addToBackStack(null);
+
+        // Commit the transaction
+        transaction3.commit();
+
+    }
 }
+
---
+++ src/main/java/group4/tcss450/uw/edu/campanion/PlaceHolderFragment.java
@@ -0,0 +1,94 @@
+/**
+ * Amanda Aldrich
+ *
+ * This is just a placeholder
+ */
+
+package group4.tcss450.uw.edu.campanion;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ *
+ * create an instance of this fragment.
+ */
+public class PlaceHolderFragment extends Fragment {
+
+
+
+    public PlaceHolderFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_place_holder, container, false);
+        return v;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+
+        if (getArguments() != null) {
+
+            if(getArguments().containsKey(getString(R.string.login_key))){
+                User used = (User) getArguments().getSerializable(getString(R.string.login_key));
+                updateLoginContent(used);
+                updatepasswordContent(used);
+            }
+            else {
+                User used = (User) getArguments().getSerializable(getString(R.string.signup_key));
+                updateLoginContent(used);
+                updatepasswordContent(used);
+
+
+            }
+        }
+    }
+
+    //updates the textView displaying login
+    public void updateLoginContent(User used) {
+        String login = used.getLogin();
+        TextView tv = (TextView) getActivity().findViewById(R.id.displayUsername);
+        tv.setText(login);
+    }
+
+    //updates the textView displaying password
+    public void updatepasswordContent(User used) {
+        String pw = used.getPassword();
+        TextView tv = (TextView) getActivity().findViewById(R.id.displayPassword);
+        tv.setText(pw);
+    }
+
+
+
+
+}
